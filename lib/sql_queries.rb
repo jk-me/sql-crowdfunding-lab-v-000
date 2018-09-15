3e9c0fbcd2d685bdb8856bd9954be4aa22658552
Jenny K <jkam17@gmail.com>
2018-09-15 20:20:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,9 @@ group by users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"select project.titles, (sum(pledges.amount)-project.funding goal) as 'over_goal'
+"select project.titles, (sum(pledges.amount)-project.funding goal) as over
 from projects join pledges on projects.id = pledges.project_id
-where 'over_goal' > 0"
+where over > 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
